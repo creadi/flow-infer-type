cee76c62386c60f3165b97e586bd04927252b04b
syzer3@gmail.com <syzer3@gmail.com>
2018-01-08 11:56:35 +0100

    Rewritten to ramda

--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,21 @@
 const _ = require('lodash');
 const R = require('ramda');
 
-// toFlowType :: Object => String
-const toFlowType = obj => JSON.stringify(obj, null, 2);
+// serialize :: Object => String
+const serialize = obj => JSON.stringify(obj, null, 2);
 const cleanup = str => str.replace(/"/g, '');
 const walk = (func, obj) => JSON.parse(JSON.stringify(obj), func);
 
-const createFlowType = obj => cleanup(toFlowType(walk((key, value) => {
-  if (R.is(Number, value)) {
-    return 'number';
-  }
-  if (R.is(Boolean, value)) {
-    return 'boolean';
-  }
-  if (R.isEmpty(value)) {
-    return 'Object';
-  }
-  if (R.is(String, value)) {
-    return 'string';
-  }
-  // Here maybe check if this is flat array
-  if (Array.isArray(value)) {
-    return `Array<${mostCommonValueInArr(value)}>`;
-  }
+const createFlowType = obj => cleanup(serialize(walk(mapType, obj)));
 
-  return value;
-}, obj)));
+const mapType = (key, value) => R.cond([[R.is(Number), R.always('number')], [R.is(Boolean), R.always('boolean')], [R.isEmpty, R.always('Object')], [R.is(String), R.always('string')], [Array.isArray, value => `Array<${findMode(value)}>`], [R.T, R.always(value)]])(value);
 
 const inferType = R.cond([[R.isNil, R.always('void')], [_.isInteger, R.always('integer')], [R.is(Number), R.always('number')], [_.isDate, R.always('date')], [R.is(String), R.always('string')], [R.T, R.always('mixed')]]);
 
-const mostCommonValueInArr = R.pipe(R.countBy(R.identity), R.toPairs(), R.sortBy(R.tail), R.takeLast(1), R.map(R.head), R.head);
+// findMode :: Array<T> => T
+const findMode = R.pipe(R.countBy(R.identity), R.toPairs(), R.sortBy(R.tail), R.takeLast(1), R.map(R.head), R.head);
 
-const mostCommonTypeInAr = R.pipe(R.map(inferType), mostCommonValueInArr);
+const mostCommonTypeInAr = R.pipe(R.map(inferType), findMode);
 
 module.exports = {
   createFlowType,
