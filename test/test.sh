f3ffedf5010b553fc805b4c1b85478b4ac7a32f9
syzer3@gmail.com <syzer3@gmail.com>
2018-01-08 14:01:52 +0100

    Add shell integration tests

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Tests for shell integration
+
 # JSON => flow type
 echo '{"a": "ssss"}' | dist/bin.js
 
 # JS object => flow type
 echo '{a: "ssss"}' | dist/bin.js
+
+echo '{a: 42}' | dist/bin.js
