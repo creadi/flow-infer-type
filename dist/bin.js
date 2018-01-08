cd9e3025c4fbb39aae8f95dc90133147a82dcd08
syzer3@gmail.com <syzer3@gmail.com>
2018-01-08 15:31:01 +0100

    test precommit

--- dist/bin.js
+++ dist/bin.js
@@ -19,6 +19,6 @@ const parse = str => {
 
 let chunks = '';
 process.stdin.resume();
-process.stdin.on('data', data => chunks += data.toString());
+process.stdin.on('data', data => chunks += data.toString())
 
 process.stdin.on('end', () => console.log(createFlowType(parse(chunks))));
